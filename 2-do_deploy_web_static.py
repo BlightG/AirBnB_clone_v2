c5ec8ec630e2f4dba4c5b06b56a09fc09ba0027e
BlightG <birhane.gulilat@gmail.com>
2023-01-23 23:19:56 +0300

    first push attempt

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' a script to distrbitue an arcive to the web servers listed'''
+from fabric.api import run
+from fabric.api import put
+from fabric.api import env
+import os
+env.hosts = ['54.144.137.83', '100.25.165.152']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    ''' puts the archive file specified in archive_path
+        to the desired weserver'''
+#    env.hosts = ['54.144.137.83','100.25.165.152']
+#    env.user = 'ubuntu'
+
+#    if not isinstance(archive_path, str):
+#        return False
+
+    if os.path.exists(archive_path) is False:
+        print(archive_path)
+        return False
+
+    if put(archive_path, '/tmp').failed is True:
+        return False
+
+    run('tar -xvf /tmp/{} --directory /data/web_static/releases/'.format(
+        archive_path.split('/')[1]))
+    run('rm /tmp/{}'.format(archive_path.split('/')[1]))
+    run('rm /data/web_static/current')
+    run('ln -s /data/web_static/releases/{} /data/web_static/current'
+        .format(archive_path.replace('.tgz', '')))
+    return True
