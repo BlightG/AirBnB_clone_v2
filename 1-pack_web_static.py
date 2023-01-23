c20669115e2d93c8029076eeb036a9fba40576cf
BlightG <birhane.gulilat@gmail.com>
2023-01-23 17:03:12 +0000

    made file a variable

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 '''script to generate tgz archive from the contents of web_static'''
 from fabric.api import local
+from datetime import datetime
 import os
 
 
 def do_pack():
     '''a function to compress files'''
     if not os.path.isdir('versions'):
-        local('mkdir versions')
-    local('tar -cavf "web_static_$(date +"%Y%m%d%h%m%s").tar.gz" web_static')
-    local('mv web_static_* versions')
-    return ('versions')
+        if local('mkdir -p versions').failed is True:
+            return None
+
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if local('tar -cavf {} web_static'.format(file)).failed is True:
+        return None
+
+    return file
