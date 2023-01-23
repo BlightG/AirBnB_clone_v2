b4c0a40038867492d3aaad25d3a1c4c398be7bfd
BlightG <birhane.gulilat@gmail.com>
2023-01-23 13:25:03 +0000

    made modifications

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,7 @@ sudo apt install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "
-<html>
+echo "<html>
   <head>
   </head>
   <body>
@@ -16,12 +15,10 @@ echo "
   </body>
 </html>
 " | sudo tee /data/web_static/releases/test/index.html
-if [[ -f /data/web_static/current ]];
-then
-    rm /data/web_static/current
-fi
+test -L /data/web_static/current && sudo rm /data/web_static/current
+
 ln -s /data/web_static/releases/test/ /data/web_static/current
-new_location=" \tlocation /hbnb_static{\n\t\talias /data/web_static/current\n}"
+new_location=" \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "37i\\$new_location" /etc/nginx/sites-enabled/default
+sudo sed -i "36i\\$new_location" /etc/nginx/sites-enabled/default
 sudo service nginx restart
