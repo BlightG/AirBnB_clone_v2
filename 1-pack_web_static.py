1912e678926292774d6f53d6d0eaebe38d34a086
BlightG <birhane.gulilat@gmail.com>
2023-01-23 19:41:30 +0300

    task 1 first attempt

---
+++ 1-pack_web_static.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''script to generate tgz archive from the contents of web_static'''
+from fabric.api import local
+import os
+
+def do_pack():
+    '''a function to compress files'''
+    if not os.path.isdir('versions'):
+        local('mkdir versions')
+    local('tar -cavf "web_static_$(date +"%Y%m%d%h%m%s").tar.gz" web_static')
+    local('mv web_static_* versions')
