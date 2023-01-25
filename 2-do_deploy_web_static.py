8838582808dc0fefad096fda015748e3f88ae7ad
BlightG <birhane.gulilat@gmail.com>
2023-01-25 13:09:41 +0300

    task 2 attempt

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,23 +11,40 @@ env.user = 'ubuntu'
 def do_deploy(archive_path):
     ''' puts the archive file specified in archive_path
         to the desired weserver'''
-#    env.hosts = ['54.144.137.83','100.25.165.152']
-#    env.user = 'ubuntu'
-
-#    if not isinstance(archive_path, str):
-#        return False
 
     if os.path.exists(archive_path) is False:
         return False
 
-    if put(archive_path, '/tmp').failed is True:
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, '/tmp/{}'.format(file)).failed is True:
         return False
 
-    run('mkdir -p /data/web_static/releses/{}'.format(archive_path.split('/')[0]))
-    run('tar -xvf /tmp/{} --directory /data/web_static/releases/'.format(
-        archive_path.split('/')[1]))
-    run('rm /tmp/{}'.format(archive_path.split('/')[1]))
-    run('rm /data/web_static/current')
-    run('ln -s /data/web_static/releases/{} /data/web_static/current'
-        .format(archive_path.replace('.tgz', '')))
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+
+    if run('mkdir -p /data/web_static/releases/{}/'.
+            format(name)).failed is True:
+        return False
+
+    if run('tar -xvf /tmp/{} -C /data/web_static/releases/{}/'.format(
+           file, name)).failed is True:
+        return False
+
+    if run('rm /tmp/{}/'.format(file)).failed is True:
+        return False
+
+    if run('mv /data/web_static/releases/{}/web_static/* '
+           '/data/web_static/releases/{}/'.format(name, name)).failed is True:
+        return False
+
+    if run('rm -rf /data/web_static/current').failed is True:
+        return False
+
+    if run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
+           format(name)):
+        return False
+
     return True
