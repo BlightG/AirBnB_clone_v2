eed2d43cb4a04cc65aa81d8224dd28aceae262bb
BlightG <birhane.gulilat@gmail.com>
2023-01-26 02:10:52 +0300

    first attem for 101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,39 @@
+# task 0 using ppupet
+
+file { ['/data',
+        '/data/web_static',
+        '/data/web_static/releases',
+        '/data/web_static/shared',
+        '/data/web_static/releases/test',]:
+  ensure => 'directory',
+  before => File['/data/web_static/releases/test/index.html']
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => present,
+  content => 'Hello Holberton',
+}
+
+exec { 'test -L':
+  path    => 'usr/bin/env bash',
+  command => 'test -L /data/web_static/current && sudo rm /data/web_static/current',
+  require => File['/data/web_static/releases/test/index.html'],
+}
+
+exec { 'ln -s':
+  path    => 'usr/bin/env bash',
+  command => 'sudo ln -s /data/web_static/releases/test/ /data/web_static/current',
+  require => Exec['test -L'],
+}
+
+exec { 'sed -i':
+  path    => 'usr/bin/env bash',
+  command => 'sudo sed -i "36i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t} /etc/nginx/sites-enabled/default',
+  notify  => Exec['nginx'],
+}
+
+exec { 'nginx':
+  path    => 'usr/bin/env bash',
+  command => 'sudo service nginx restart',
+  require => Exec['sed -i'],
+}
