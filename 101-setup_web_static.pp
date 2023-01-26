720840bc1675900453e3139565b41c5217428e26
BlightG <birhane.gulilat@gmail.com>
2023-01-26 03:01:38 +0300

    task 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -4,13 +4,16 @@ package { 'nginx':
   ensure => 'present',
 }
 
-file { ['/data',
-        '/data/web_static',
-        '/data/web_static/releases',
-        '/data/web_static/shared',
-        '/data/web_static/releases/test',]:
-  ensure => 'directory',
-  before => File['/data/web_static/releases/test/index.html']
+exec { 'mkdir -p':
+  path    => ['/usr/bin','/usr/sbin/','/bin'],
+  command => 'sudo mkdir -p /data/web_static/shared/ ;sudo  mkdir -p /data/web_static/releases/test/',
+  before  => Exec['touch'],
+}
+
+exec { 'touch':
+  path    => '/usr/bin/',
+  command => 'touch /data/web_static/releases/test/index.html',
+  before  => File['/data/web_static/releases/test/index.html'],
 }
 
 file { '/data/web_static/releases/test/index.html':
@@ -19,32 +22,32 @@ file { '/data/web_static/releases/test/index.html':
 }
 
 exec { 'test -L':
-  path    => 'usr/bin/env bash',
-  command => 'test -L /data/web_static/current && sudo rm /data/web_static/current',
+  path    => '/usr/bin/',
+  command => 'test -L /data/web_static/current &&  rm /data/web_static/current',
   require => File['/data/web_static/releases/test/index.html'],
 }
 
 exec { 'ln -s':
-  path    => 'usr/bin/env bash',
-  command => 'sudo ln -s /data/web_static/releases/test/ /data/web_static/current',
+  path    => '/usr/bin/',
+  command => 'ln -s /data/web_static/releases/test/ /data/web_static/current',
   require => Exec['test -L'],
 }
 
 exec { 'chown':
-  path    => 'usr/bin/env bash',
-  command => 'sudo chown -R ubuntu:ubuntu /data',
+  path    => '/usr/bin/',
+  command => 'chown -R ubuntu:ubuntu /data',
   require => Exec['ln -s'],
 }
 
 exec { 'sed -i':
-  path    => 'usr/bin/env bash',
-  command => 'sudo sed -i "36i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t} /etc/nginx/sites-enabled/default',
+  path    => '/usr/bin/',
+  command => 'sed -i "36i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t} /etc/nginx/sites-enabled/default',
   require => Package['nginx'],
   notify  => Exec['nginx'],
 }
 
 exec { 'nginx':
-  path    => 'usr/bin/env bash',
-  command => 'sudo service nginx restart',
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
   require => Exec['sed -i'],
 }
