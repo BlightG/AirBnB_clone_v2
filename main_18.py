1e869e064ca0bb0185c4b27971ecac1d04edcd54
BlightG <birhane.gulilat@gmail.com>
2023-02-06 20:16:42 +0300

    new trial for database

--- main_18.py
+++ main_18.py
@@ -61,18 +61,21 @@ def exec_command(my_console, the_command, last_lines = 1):
 """
  Tests
 """
-result = exec_command(my_console, "all User", 4)
-if result is None or result == "":
-    print("FAIL: No users retrieved")
-if "my_id_0" not in result or "email0@gmail.com" not in result or "pwd0" not in result or "John" not in result or "Doe" not in result:
-    print("FAIL: Missing information 0")
-if "my_id_1" not in result or "email1@gmail.com" not in result or "pwd1" not in result or "Bob" not in result:
-    print("FAIL: Missing information 1")
-if "my_id_2" not in result or "email2@gmail.com" not in result or "pwd2" not in result or "Did" not in result:
-    print("FAIL: Missing information 2")
-if "my_id_3" not in result or "email3@gmail.com" not in result or "pwd3" not in result:
-    print("FAIL: Missing information 3")
-    
+state_id = exec_command(my_console, "create State name=\"California\"")
+if state_id is None or state_id == "":
+    print("FAIL: Can't create State")
+
+city_id = exec_command(my_console, "create City state_id=\"{}\" name=\"Fremont\"".format(state_id))
+if city_id is None or city_id == "":
+    print("FAIL: Can't create City")
+
+user_id = exec_command(my_console, "create User email=\"a@a.com\" password=\"pwd\" first_name=\"fn\" last_name=\"ln\"")
+if user_id is None or user_id == "":
+    print("FAIL: Can't create User")
+
+place_id = exec_command(my_console, "create Place city_id=\"{}\" user_id=\"{}\" name=\"House\" description=\"des\" number_rooms=4 number_bathrooms=2 max_guest=6 price_by_night=100 latitude=1.3 longitude=2.3".format(city_id, user_id))
+if place_id is None or place_id == "":
+    print("FAIL: Can't create Place")
 print("OK", end="")
 
 shutil.copy("tmp_console_main.py", "console.py")
